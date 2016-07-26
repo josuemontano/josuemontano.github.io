ae36a7a759e78483c982af1886be5a296552bba2
Josue Montano <josuemontanoa@gmail.com>
2016-07-26 18:03:00 -0400

    Add uglify transformation to browserify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function (grunt) {
                         '\n * @version <%= pkg.version %>' +
                         '\n * @date <%= grunt.template.today("dd-mm-yyyy") %>' +
                         '\n**/\n',
-                    transform: ['coffeeify', 'hbsfy'],
+                    transform: ['coffeeify', 'hbsfy', 'uglifyify'],
                 }
             },
         },
