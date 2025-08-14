694a486c43897745450bf9ce82e158c2b4e8ac10
Josue Montano <josuemontanoa@gmail.com>
2025-08-14 16:35:56 -0400

    chore: create basic theme

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Modo Moves the Mind";
+export const SITE_DESCRIPTION = "A tiny portal into my perspective on software and its workings";
