8514bb23e8a51342a2186c13eb3588856c67b562
Josue Montano <josuemontanoa@gmail.com>
2025-08-14 22:29:56 -0400

    chore: update blog description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Modo Moves the Mind";
-export const SITE_DESCRIPTION = "A tiny portal into my perspective on software and its workings";
+export const SITE_DESCRIPTION =
+  "A tiny portal into my perspective on software, its workings, and everything in between";
