aa0182578867518f9f9f3a5c9a6bc8eed08c82ee
Josue Montano <josuemontanoa@gmail.com>
2025-08-27 11:02:55 -0400

    chore: update blog title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Modo Moves the Mind";
+export const SITE_TITLE = "Josue Montano's Blog";
 export const SITE_DESCRIPTION =
   "A tiny portal into my perspective on software, its workings, and everything in between";
